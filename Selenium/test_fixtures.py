2ac63044a9730465adb4996099d1ff5c0eb05044
Yurii Borodylin <borodylin3@gmail.com>
2025-10-13 10:45:06 +0300

    создание тестов

---
+++ Selenium/test_fixtures.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.fixture
+def login_page(browser):
+    print("логин пейдж!")
+    pass
+
+@pytest.fixture
+def user():
+    print("Юзер!")
+    return "username", "password"
+
+def test_login(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
+
+
+def test_logout(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
