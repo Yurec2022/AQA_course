cda5bda400e79323009c359cf08c64da75ce03f4
Yurii Borodylin <borodylin3@gmail.com>
2026-01-22 14:35:23 +0200

    first test

--- test_login/conftest.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-#
-#
-# @pytest.fixture(scope="session")
-# def browser():
-#     print("Браузер!")
-#     yield
-#     print("закрыть браузер")
--- test_login/test_ui.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import sync_playwright, Page
-
-def test_mane_page(page:Page):
-    page.goto("https://aqa-proka4.org/sandbox/web")
-    assert page.title() == "WEB Sandbox - Практика автоматизации"
